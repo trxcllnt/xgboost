94718e1fc0618c23748202f253aa893ec8a293aa
Tim Liu <timl@nvidia.com>
2019-07-10 15:11:30 +0800

    Revert "Temporarily skip test for nightly CI"
    This reverts commit d2f92e0e2fbd9132fbc3aa201a9cfc95779a4852.

--- tests/ci_build/build_jvm_deploy.sh
+++ tests/ci_build/build_jvm_deploy.sh
@@ -16,9 +16,9 @@ pushd jvm-packages
 mvn clean
 if [ $1 == "10.0" ]; then
     echo "mvn deploy for cuda10.0"
-    mvn -Dmaven.repo.local=$WORKDIR/.m2 -DskipTests package deploy
+    mvn -Dmaven.repo.local=$WORKDIR/.m2 package deploy
 else
     echo "mvn deploy for cuda9.2"
-    mvn -Dmaven.repo.local=$WORKDIR/.m2-9.2 -DskipTests -Pcuda9.2 package deploy
+    mvn -Dmaven.repo.local=$WORKDIR/.m2-9.2 -Pcuda9.2 package deploy
 fi
 popd
