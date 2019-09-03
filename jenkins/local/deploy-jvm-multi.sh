fc94fdd717bca19b0513b0371d9a424369131072
Liangcai Li <liangcail@nvidia.com>
2019-09-03 01:25:02 -0700

    Updated the scripts for public deployment.
    We have involved a new build process for XGBoost JVM to build a single
    jar with both cuda9.2 and cuda10.0 `.so` files inside.
    
    So update the build/deploy/jenkins scripts accordingly.
    
    And add the tmp pom file into .gitignore.

--- jenkins/local/deploy-jvm-multi.sh
+++ jenkins/local/deploy-jvm-multi.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 ##
 #
-# Script to deploy xgboost jar files along with other classifiers,
-# such as cuda10, centos, centos-cuda10, sources, javadoc.
+# Script to deploy xgboost jar files along with doc and sources jar,
 #
 ###
 set -e
 
 echo "Jenkins UID: $JENKINS_UID"
-echo "Build Centos?: $BUILD_CENTOS7"
 echo "Sign Jar?: $1"
 SIGN_FILE=$1
-
+###### Build deploy command ######
 if [ "$SIGN_FILE" == true ]; then
     DEPLOY_CMD="mvn gpg:sign-and-deploy-file -s settings.xml -Dgpg.passphrase=$GPG_PASSPHRASE"
 else
@@ -20,33 +18,9 @@ fi
 DEPLOY_CMD="$DEPLOY_CMD -Durl=$SERVER_URL -DrepositoryId=$SERVER_ID"
 echo "Deploy cmd: $DEPLOY_CMD"
 
-if [ "$BUILD_CENTOS7" == true ]; then
-    CLASSIFIERS="cuda10,cuda9-centos7,cuda10-centos7"
-    TYPES="jar,jar,jar"
-else
-    CLASSIFIERS=cuda10
-    TYPES=jar
-fi
-
-deploySubModule()
-{
-    FILE_LIST=()
-    FPATH="$OUT/$1/$1_$SCALA_BIN_VERSION-$REL_VERSION"
-    for CLS in $(IFS=, ; echo $CLASSIFIERS); do
-        FILE_LIST+=("$FPATH-$CLS.jar")
-    done
-    FILES=$(IFS=, ; echo "${FILE_LIST[*]}")
-    echo "Additional files: $FILES"
-    if [ "$SIGN_FILE" == true ]; then
-        SRC_DOC_JARS="-Dsources=$FPATH-sources.jar -Djavadoc=$FPATH-javadoc.jar"
-    fi
-    $DEPLOY_CMD $SRC_DOC_JARS \
-        -Dfile=$FPATH.jar -DpomFile=$1/pom.xml \
-        -Dfiles=$FILES -Dtypes=$TYPES -Dclassifiers=$CLASSIFIERS
-}
-
 cd jvm-packages
 
+###### Build jar information ######
 REL_VERSION=`mvn exec:exec -q --non-recursive \
     -Dexec.executable=echo \
     -Dexec.args='${project.version}'`
@@ -57,10 +31,20 @@ SCALA_BIN_VERSION=`mvn exec:exec -q --non-recursive \
     -Dexec.args='${scala.binary.version}'`
 echo "scala binary version = $SCALA_BIN_VERSION"
 
-# Deploy parent pom file of jvm-packages
+###### Start deployment ######
+deploySubModule()
+{
+    FPATH="$OUT/$1/$1_$SCALA_BIN_VERSION-$REL_VERSION"
+    if [ "$SIGN_FILE" == true ]; then
+        SRC_DOC_JARS="-Dsources=$FPATH-sources.jar -Djavadoc=$FPATH-javadoc.jar"
+    fi
+    $DEPLOY_CMD -Dfile=$FPATH.jar -DpomFile=$1/pom.xml $SRC_DOC_JARS
+}
+
+# 1) Deploy parent pom file of jvm-packages
 $DEPLOY_CMD -Dfile=./pom.xml -DpomFile=./pom.xml
 
-# deploy sub modules
+# 2) Deploy sub modules
 deploySubModule xgboost4j
 deploySubModule xgboost4j-spark
 
